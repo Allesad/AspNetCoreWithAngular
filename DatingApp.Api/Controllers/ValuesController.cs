40fd99cb4d73765bbe900fb4c810ecd04759a2f5
Alexey Sakhno <alexey.sakhno@etna.etnasoft.com>
2018-09-06 18:20:14 +0300

    Playing with DataContext

--- DatingApp.Api/Controllers/ValuesController.cs
+++ DatingApp.Api/Controllers/ValuesController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.Api.Data;
+using DatingApp.Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.Api.Controllers
 {
@@ -10,24 +13,43 @@ namespace DatingApp.Api.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _ctx;
+
+        public ValuesController(DataContext ctx)
+        {
+            this._ctx = ctx;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(await _ctx.Values.ToListAsync());
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
-            return "value";
+            var value = await _ctx.Values.FirstOrDefaultAsync(v => v.Id == id);
+            if (value == null) return NotFound();
+
+            return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            var lastId = _ctx.Values.AsQueryable()
+                .OrderByDescending(v => v.Id)
+                .Select(v => v.Id)
+                .FirstOrDefault();
+
+            _ctx.Values.Add(new Value{
+                Id = lastId + 1,
+                Name = value
+            });
         }
 
         // PUT api/values/5
