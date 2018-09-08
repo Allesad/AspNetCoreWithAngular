43d0b84caa64cf6777b8e68c33c9dbe368795211
Allesad <allesad.dev@gmail.com>
2018-09-09 02:05:08 +0300

    Added member-list and member-card components

--- DatingApp-Spa/src/app/app.module.ts
+++ DatingApp-Spa/src/app/app.module.ts
@@ -13,12 +13,13 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 export function tokenGetter() {
     return localStorage.getItem('token');
@@ -32,7 +33,8 @@ export function tokenGetter() {
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent
    ],
    imports: [
       BrowserModule,
