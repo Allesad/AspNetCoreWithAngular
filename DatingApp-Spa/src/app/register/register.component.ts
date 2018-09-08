0744b84ae4e4efe129951809e4d34ab5a2853c9d
Allesad <allesad.dev@gmail.com>
2018-09-08 12:22:11 +0300

    Added alertifyjs and JWT token helper

--- DatingApp-Spa/src/app/register/register.component.ts
+++ DatingApp-Spa/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -13,21 +14,20 @@ export class RegisterComponent implements OnInit {
   cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit() {}
 
   register() {
     this.authService.register(this.model).subscribe(
       () => {
-        console.log('Registered');
+        this.alertify.success('Registration successful');
       },
-      err => console.error(err)
+      err => this.alertify.error(err)
     );
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
   }
 }
